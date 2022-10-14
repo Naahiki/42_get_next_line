f3ac64f263904c37f8f58688a2c218c1e4378f4a
Nahikari Rodriguez Nogales <nrodrigu@c2r3s3.42urduliz.com>
2022-10-14 13:02:09 +0200

    FirstSteps

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 12:57:27 by nrodrigu          #+#    #+#             */
+/*   Updated: 2022/10/14 13:00:20 by nrodrigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+
+#endif
